6ebc0e0d30b0f9a8502f29074aac5eb49ea64eac
Michael Chappel <31025057+mchappeldev@users.noreply.github.com>
2022-09-08 14:42:09 -0400

    Update hooks.tsx

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 //Interfaces
 import { Article } from "./Interfaces";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export function useArticleFetch(selectedCategory: string) {
   const [articles, setArticles] = useState([]);
